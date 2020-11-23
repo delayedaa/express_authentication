8ea90769ae5f07673ff3d7cf75f3dad43c3bca2a
delayedaa <delayedaa@gmail.com>
2020-11-23 15:28:10 -0600

    [user.test.js] Modify test for valid password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+let db = require('../models');
+let passport = require('../config/ppConfig');
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -7,6 +9,49 @@ router.get('/signup', (req, res) => {
 
 router.get('/login', (req, res) => {
   res.render('auth/login');
+});
+
+router.post('/signup', (req, res) => {
+  console.log(req.body);
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).then(([user, created]) => {
+    if (created) {
+      // if created, success and redirect back to home
+      console.log(`${user.name} was created`);
+      // Flash Message
+      let successObject = {
+        successRedirect: '/',
+        successFlash: 'Account created and logged in'
+      };
+      passport.authenticate('local', successObject)(req, res);
+    } else {
+      req.flash('error', 'Email already exists...');
+      res.redirect('/auth/signup');
+    }
+  }).catch(error => {
+    console.log('Error', error);
+    req.flash('error', 'Either email or password is incorrect. Please try again.');
+    res.redirect('/auth/signup');
+  });
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login',
+  failureFlash: 'Invalid username and/or password',
+  successFlash: 'You have logged in'
+}));
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  // FLASH
+  req.flash('success', 'You have logged out');
+  res.redirect('/');
 });
 
 module.exports = router;
